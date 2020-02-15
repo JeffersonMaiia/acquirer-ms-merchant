193188563697a5a563d60a8fe1951b2ff8711925
Jefferson Maia <jefferson.maia@cabal.com.br>
2020-02-14 21:32:32 -0300

    first commit

---
+++ src/main/java/cabal/acquirer/ms/merchant/AcquirerMsMerchantApplication.java
@@ -0,0 +1,13 @@
+package cabal.acquirer.ms.merchant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcquirerMsMerchantApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AcquirerMsMerchantApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/cabal/acquirer/ms/merchant/AcquirerMsMerchantApplicationTests.java
@@ -0,0 +1,13 @@
+package cabal.acquirer.ms.merchant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcquirerMsMerchantApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
